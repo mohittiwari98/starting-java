27e00877d825624a2be0cd5c7c471627eb60ee6b
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 21:55:53 +0530

    reverse_number.java

---
+++ reverse_number.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(string[] args){
+        int num=1234,reversed=0;
+        System.out.println("orginal num"+num);
+        while(num!=0){
+            int digit=num%10;
+            reversed=reversed*10+digit;
+            num/=10;
+        }
+        System.out.println("reversed number:"+reversed);
+    }
+}
