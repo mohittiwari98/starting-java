cb736d4a811255db15c11fd9eca4c6f35f97f13f
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 21:52:08 +0530

    check leap year.java

---
+++ check leap year.java
@@ -0,0 +1,16 @@
+public class  main{
+    public static void main(string[] args){
+        int year=100;
+        boolean leap=false;
+        if(year%4==0){
+            if(year%100=0){
+                if(year%400==0){
+                    leap =true;
+                    else{
+                        leap=false;
+                    }
+                }
+            }
+        }
+    }
+}
