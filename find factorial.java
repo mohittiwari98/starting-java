546c2505915e1da008469c2fb7bc410ddb668be2
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 22:43:32 +0530

    find factorial.java

--- find factorial.java
+++ find factorial.java
@@ -3,6 +3,7 @@ public class factorial{
     public static void main(string[] args){
         int num=10;
         long factorial=1;
+        
         for(int i=1; i<=num; ++i)
         {
             factorial*=i;
