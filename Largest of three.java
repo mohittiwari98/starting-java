86dc0308397b0471b9aefb0ff617797240469ab1
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 21:54:19 +0530

    Largest of three.java

---
+++ Largest of three.java
@@ -0,0 +1,12 @@
+public class Largest{
+    public static void main(string[] args){
+        double n1=4.5,n2=0.9,n3=9.0;
+        if(n1>=n2 && n1>=n3)
+            System.out.println(n1+"is largest");
+        else if(n2>=n1 && n2>=n3)
+            System.out.println(n2+"is largest");
+        else{
+            System.out.println(n3+"is largest");
+        }
+    }
+}
