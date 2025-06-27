d9d6cf17bbb4101d9ebc94597e01b92917c38a6f
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 21:57:32 +0530

    vowel or non vowel.java

---
+++ vowel or non vowel.java
@@ -0,0 +1,9 @@
+public class vowelquoinent{
+    public static voi main(String[] args){
+        char ch='i';
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        System.out.println(ch +"is vowel");
+    else
+        System.out.println(ch +"is not vowel");
+    }
+}
