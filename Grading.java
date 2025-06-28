20d917874919e4cabfefa4c64fc89202c5404d50
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 13:21:39 +0530

    Grading.java

---
+++ Grading.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Grading{
+    public static void main(string[] args){
+        Scanner input = new Scanner(system.in);
+        System.out.println("welcome\n");
+        System.out.print("please enter your percentage");
+        float percentage=input.nextFloat();
+
+        if(percentage>=90){
+            System.out.println("A");
+        } else if(percentage>=75){
+            System.out.println("B");
+        } else if(percentage>=60){
+            System.out.println("C")
+        }
+        else{
+            System.out.println("D")
+        }
+    }
+}
