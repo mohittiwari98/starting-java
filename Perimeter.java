8fbef21d2a6b2c2ca13e4399a20b1f01c096ccf2
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 13:22:31 +0530

    Perimeter.java

---
+++ Perimeter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Perimeter{
+    public static void main(string[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("welcome to perimeter calculator");
+        System.out.print("please enter all 4 sides in cms");
+        double a=input.nextDouble();
+        double b=input.nextDouble();
+        double c=input.nextDouble();
+        double d=input.nextDouble();
+
+        double perimeter=a+b+c+d;
+        System.out.println(perimeter);
+    }
+}
