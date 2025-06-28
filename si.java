7604c0df5d3b2eff565a2b819f81c6c517d1bc1e
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 14:15:29 +0530

    si.java

--- si.java
+++ si.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 class si{
     public static void main(string[] args){
-        //input value her
+        //input value here
         Scanner input=new Scanner(system.in);
         System.out.println("welcome  to si calculator");
         System.out.print("enter your principle");
