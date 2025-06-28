454ded3cd212f8005dbaa9ee591e3f6815f8c825
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 13:49:59 +0530

    Ascii.java

--- Ascii.java
+++ Ascii.java
@@ -1,7 +1,7 @@
 //find ascii value
 public class AsciiValue{
     public static void main(String[] args){
-        //change alphabet 
+        //change alphabet to
         char ch='a';
         int asccii=ch;
         int castAscii=(int) ch;
