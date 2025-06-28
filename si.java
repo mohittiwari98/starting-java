800445926603ca9c7f10510a9b9f10a0a8b4fc04
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 13:20:40 +0530

    si.java

---
+++ si.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class si{
+    public static void main(string[] args){
+        Scanner input=new Scanner(system.in);
+        System.out.println("welcome  to si calculator");
+        System.out.print("enter your principle");
+        int principle=input.nextInt();
+         System.out.print("enter your rate");
+        Float rate=input.nextFloat();
+         System.out.print("enter time");
+        int time=input.nextInt();
+
+        float intreset=(principle*rate*years)/100;
+        System.out.println(intreset)
+    }
+}
