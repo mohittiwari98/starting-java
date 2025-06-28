dd90bf475e9fa37f3cfcbe67564db2d50e4196e4
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-28 13:20:02 +0530

    Trainglearea.java

---
+++ Trainglearea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Traingle{
+    public static void main(string[] args){
+        Scanner input=new Scanner(system.in);
+        System.out.println("welcome to area calculator\n");
+        System.out.print("please enter your base in cms");
+        double.base=input.nextDouble();
+        System.out.print("enter your height");
+        double height=input.nextDouble();
+
+        double area=(base*height)/2;
+
+        System.out.println(area)
+    }
+}
