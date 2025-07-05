86f90fe9d72fdf8e97a279d3aec000b430bea73a
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-05 09:35:02 +0530

    Ascii.java

--- Ascii.java
+++ Ascii.java
@@ -5,7 +5,7 @@ public class AsciiValue{
         char ch='a';
         int asccii=ch;
         int castAscii=(int) ch;
-        //
+        //g
         System.out.println("the ascii is"+ch+"is"+ascii);
     }
 }
