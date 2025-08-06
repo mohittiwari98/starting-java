f0d57df6fc8755364ba2fee95ab0218e89d33632
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-08-06 18:52:04 +0530

    Update reverse_number.java

--- reverse_number.java
+++ reverse_number.java
@@ -1,7 +1,7 @@
 //code to get reverse of number
 class Main{
     public static void main(string[] args){
-       //use your ow
+       //use your own
         int num=1234,reversed=0;
         System.out.println("orginal num"+num);
         while(num!=0){
