141471ca605a20ca46ae5a92512b148f4d6631f6
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-08-07 13:08:19 +0530

    Update vowel or non vowel.java

--- vowel or non vowel.java
+++ vowel or non vowel.java
@@ -1,7 +1,7 @@
 //find the vowel and constent  
 public class vowelquoinent{
     public static voi main(String[] args){
-        //use your 
+        //use your  
         char ch='i';
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
         System.out.println(ch +"is vowel");
