8f804c53a02f23aaab2d5277b90847d064ff6798
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-08-18 22:00:01 +0530

    Update Binary search.java

--- Binary search.java
+++ Binary search.java
@@ -1,6 +1,6 @@
 //Binary search in java using recursion 
 //using recursive approach 
-//implemente
+//implemented
 class BinarySearch {
   int binarySearch(int array[], int x, int low, int high) {
 
